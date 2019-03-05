fixed backprop and updated notebooks

mandatory/m01/dnn/import_data.py
@@ -81,9 +81,11 @@ def load_mnist(data_dir="data/mnist", devel_size=10000):
         return np.asarray(data, dtype=np.uint8)
 
     print("Loading MNIST data from ", data_dir)
-    X_train = _load_data('train-images-idx3-ubyte.gz', data_dir, 16).reshape((-1, 784)).T
+    X_train = _load_data('train-images-idx3-ubyte.gz',
+                         data_dir, 16).reshape((-1, 784)).T
     Y_train = _load_data('train-labels-idx1-ubyte.gz', data_dir, 8)
-    X_test = _load_data('t10k-images-idx3-ubyte.gz', data_dir, 16).reshape((-1, 784)).T
+    X_test = _load_data('t10k-images-idx3-ubyte.gz',
+                        data_dir, 16).reshape((-1, 784)).T
     Y_test = _load_data('t10k-labels-idx1-ubyte.gz', data_dir, 8)
 
     # Scale data to [0.0, 1.0]
@@ -151,11 +153,12 @@ def load_cifar10(data_dir="data/cifar10", devel_size=10000):
     else:
         print("Extraction of compression format is not implemented")
 
-    #Unpickle file and fill in data
+    # Unpickle file and fill in data
     X_train = None
     Y_train = []
     for ind in range(1, 6):
-        filepath = os.path.join(data_dir, "cifar-10-batches-py/data_batch_{}".format(ind))
+        filepath = os.path.join(
+            data_dir, "cifar-10-batches-py/data_batch_{}".format(ind))
         with open(filepath, 'rb') as fil:
             data_dict = pickle.load(fil, encoding='latin-1')
         if ind == 1:
@@ -183,6 +186,7 @@ def load_cifar10(data_dir="data/cifar10", devel_size=10000):
     Y_train, Y_devel = Y_train[:-devel_size], Y_train[-devel_size:]
 
     return X_train, Y_train, X_devel, Y_devel, X_test, Y_test
+
 
 def load_svhn(data_dir, devel_size=10000):
     """Load the Street View House Numbers (SVHN) dataset

mandatory/m01/dnn/main.py
@@ -32,20 +32,33 @@ import run
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-def config():
-    """Return a dict of configuration settings used in the program"""
+
+def config(
+        dataset,
+        hidden_dimensions,
+        batch_size,
+):
+    """
+    Return a dict of configuration settings used in the program
+
+    Input: 
+        dataset - a str of either mnist, cifar10 or svhn 
+        hidden_dimension - a list of ints denoting width and depth of layers
+
+
+    """
 
     conf = {}
 
-    # Determine what dataset to run on. 'mnist', 'cifar10' and 'svhn' are currently supported.
-    conf['dataset'] = 'mnist'
+    # Determine what dataset to run on. 'mnist', 'cifar10' and 'svhn' are currently supported
+    conf['dataset'] = dataset
     # Relevant datasets will be put in the location data_root_dir/dataset.
     conf['data_root_dir'] = "/tmp/data"
 
     # Number of input nodes. This is determined by the dataset in runtime.
     conf['input_dimension'] = None
     # Number of hidden layers, with the number of nodes in each layer.
-    conf['hidden_dimensions'] = [128, 32]
+    conf['hidden_dimensions'] = hidden_dimensions
     # Number of classes. This is determined by the dataset in runtime.
     conf['output_dimension'] = None
     # This will be determined in runtime when input_dimension and output_dimension is set.
@@ -59,7 +72,7 @@ def config():
     # pass of a mini-batch
     conf['max_steps'] = 2000
     # The batch size used in training.
-    conf['batch_size'] = 128
+    conf['batch_size'] = batch_size
     # The step size used by the optimization routine.
     conf['learning_rate'] = 1.0e-2
 
@@ -73,18 +86,22 @@ def config():
 
     return conf
 
+
 def plot_progress(train_progress, devel_progress, out_filename=None):
     """Plot a chart of the training progress"""
 
     fig, ax1 = plt.subplots(figsize=(8, 6), dpi=100)
-    ax1.plot(train_progress['steps'], train_progress['ccr'], 'b', label='Training set ccr')
-    ax1.plot(devel_progress['steps'], devel_progress['ccr'], 'r', label='Development set ccr')
+    ax1.plot(train_progress['steps'],
+             train_progress['ccr'], 'b', label='Training set ccr')
+    ax1.plot(devel_progress['steps'], devel_progress['ccr'],
+             'r', label='Development set ccr')
     ax1.set_xlabel('Steps')
     ax1.set_ylabel('Correct classification rate')
     ax1.legend(loc='lower left', bbox_to_anchor=(0.6, 0.52), framealpha=1.0)
 
     ax2 = ax1.twinx()
-    ax2.plot(train_progress['steps'], train_progress['cost'], 'g', label='Training set cost')
+    ax2.plot(train_progress['steps'], train_progress['cost'],
+             'g', label='Training set cost')
     ax2.set_ylabel('Cross entropy cost')
     gl2 = ax2.get_ygridlines()
     for gl in gl2:
@@ -99,6 +116,7 @@ def plot_progress(train_progress, devel_progress, out_filename=None):
         plt.savefig(out_filename)
 
     plt.show()
+
 
 def get_data(conf):
     """Return data to be used in this session.
@@ -139,30 +157,34 @@ def get_data(conf):
         print("Train dataset:")
         print("  shape = {}, data type = {}, min val = {}, max val = {}".format(X_train.shape,
                                                                                 X_train.dtype,
-                                                                                np.min(X_train),
+                                                                                np.min(
+                                                                                    X_train),
                                                                                 np.max(X_train)))
         print("Development dataset:")
         print("  shape = {}, data type = {}, min val = {}, max val = {}".format(X_devel.shape,
                                                                                 X_devel.dtype,
-                                                                                np.min(X_devel),
+                                                                                np.min(
+                                                                                    X_devel),
                                                                                 np.max(X_devel)))
         print("Test dataset:")
         print("  shape = {}, data type = {}, min val = {}, max val = {}".format(X_test.shape,
                                                                                 X_test.dtype,
-                                                                                np.min(X_test),
+                                                                                np.min(
+                                                                                    X_test),
                                                                                 np.max(X_test)))
 
     return X_train, Y_train, X_devel, Y_devel, X_test, Y_test
 
 
-def main():
+def main(dataset, hidden_dimensions, batch_size):
     """Run the program according to specified configurations."""
 
-    conf = config()
+    conf = config(dataset, hidden_dimensions, batch_size)
 
     X_train, Y_train, X_devel, Y_devel, X_test, Y_test = get_data(conf)
 
-    params, train_progress, devel_progress = run.train(conf, X_train, Y_train, X_devel, Y_devel)
+    params, train_progress, devel_progress = run.train(
+        conf, X_train, Y_train, X_devel, Y_devel)
 
     plot_progress(train_progress, devel_progress)
 
@@ -178,6 +200,7 @@ def main():
     num_correct, num_evaluated = run.evaluate(conf, params, X_test, Y_test)
     print("CCR = {0:>5} / {1:>5} = {2:>6.4f}".format(num_correct, num_evaluated,
                                                      num_correct/num_evaluated))
+
 
 if __name__ == "__main__":
     main()

mandatory/m01/dnn/model.py
@@ -49,10 +49,10 @@ def initialization(conf):
 
     for i in range(len(layer_dimensions) - 1):
         layer_shape = (layer_dimensions[i], layer_dimensions[i + 1])
-        layer_var = 2/layer_shape[0]
+        layer_var = 2/layer_dimensions[i]
 
-        weights = np.random.normal(0, layer_var, layer_shape)
-        bias = np.zeros((layer_shape[1]))
+        weights = np.random.normal(0, np.sqrt(layer_var), layer_shape)
+        bias = np.zeros((layer_shape[1], 1))
 
         params["W_{}".format(i+1)] = weights
         params["b_{}".format(i+1)] = bias
@@ -85,7 +85,7 @@ def activation(Z, activation_function):
         return None
 
 
-def softmax(Z):
+def softmax(Z, axis=0):
     """Compute and return the softmax of the input.
 
     To improve numerical stability, we do the following
@@ -102,7 +102,7 @@ def softmax(Z):
 
     # again, in-place modification for speed
     x = np.exp(Z)
-    normalization = np.sum(x, axis=1, keepdims=True)
+    normalization = np.sum(x, axis=axis, keepdims=True)
 
     return x/normalization
 
@@ -132,27 +132,28 @@ def forward(conf, X_batch, params, is_training, features=None):
 
     features = {}
     num_iter = len(conf["layer_dimensions"]) - 2
-    x = X_batch.T
+    x = X_batch
+
+    features["A_0"] = X_batch
 
     for i in range(num_iter):
         b = params["b_{}".format(i + 1)]
         w = params["W_{}".format(i + 1)]
 
-        z_tmp = np.matmul(x, w) + np.squeeze(b)
+        z_tmp = np.einsum("ib, io -> ob", x, w) + b
         a_tmp = activation(z_tmp, conf["activation_function"])
-        features["Z_{}".format(i + 1)] = z_tmp.T
-        features["A_{}".format(i + 1)] = a_tmp.T
+        features["Z_{}".format(i + 1)] = z_tmp
+        features["A_{}".format(i + 1)] = a_tmp
 
         x = a_tmp
 
     b = params["b_{}".format(num_iter + 1)]
     w = params["W_{}".format(num_iter + 1)]
 
-    z_tmp = np.matmul(x, w) + np.squeeze(b)
-    features["Z_{}".format(num_iter + 1)] = z_tmp.T
+    z_tmp = np.einsum("ib, io -> ob", x, w) + b
+    features["Z_{}".format(num_iter + 1)] = z_tmp
 
-    Y_proposed = softmax(z_tmp).T
-
+    Y_proposed = softmax(z_tmp)
     return Y_proposed, features
 
 
@@ -170,16 +171,17 @@ def cross_entropy_cost(Y_proposed, Y_reference, treshold=0.5):
     """
     # TODO: Task 1.3
     log_y_prop = np.log(Y_proposed)
-    num_samples = Y_proposed.shape[0]
+    num_samples = Y_proposed.shape[1]
 
     cost = -(1/num_samples) * \
         np.einsum("ij, ij -> ...", Y_reference, log_y_prop)
 
-    tmp = Y_proposed == Y_proposed.max(axis=1, keepdims=True)
-    pred_labels = np.where(tmp)[1]
+    tmp = Y_proposed == Y_proposed.max(axis=0, keepdims=True)
+    tmp = tmp.astype(np.int8)
 
-    correct_labels = np.where(Y_reference == 1)[1]
+    pred_labels = np.nonzero(tmp.T)[1]
 
+    correct_labels = np.nonzero(Y_reference.T)[1]
     num_correct = np.sum(pred_labels == correct_labels)
 
     return cost, num_correct
@@ -230,11 +232,11 @@ def backward(conf, Y_proposed, Y_reference, params, features):
     a_prev = features["A_{}".format(n_layers - 1)]
 
     dEdY = Y_proposed - Y_reference
-    dEdX_cur = dEdY * 1  # activation_derivative(zM, activation_function)
+    dEdX_cur = dEdY  # activation_derivative(zM, activation_function)
     dXdW_cur = a_prev
 
     dEdW_cur = np.einsum("ki, ji -> jk", dEdX_cur, dXdW_cur)
-    dEdB_cur = np.expand_dims(np.einsum("ij -> i", dEdX_cur) * 1, axis=1)
+    dEdB_cur = np.expand_dims(np.einsum("ij -> i", dEdX_cur), axis=1)
 
     grad_params["grad_W_{}".format(n_layers)] = dEdW_cur/batch_size
     grad_params["grad_b_{}".format(n_layers)] = dEdB_cur/batch_size
@@ -250,11 +252,14 @@ def backward(conf, Y_proposed, Y_reference, params, features):
             features["Z_{}".format(la)], activation_function)
         dXldWl = features["A_{}".format(la-1)]
 
+        #print(dEdX_cur.shape, dXlpdAl.shape)
         dEdAl = np.einsum("ki, jk -> ji", dEdX_cur, dXlpdAl)
         dEdXl = dEdAl * dAldXl
+        #print(dXldWl.shape, dEdXl.shape)
         dEdWl = np.einsum("ji, hi -> hj", dEdXl, dXldWl)
         dEdBl = np.expand_dims(np.einsum("ij -> i", dEdXl), axis=1)
 
+        #print(dEdWl.shape, params["W_"+str(la)].shape)
         grad_params["grad_W_{}".format(la)] = dEdWl/batch_size
         grad_params["grad_b_{}".format(la)] = dEdBl/batch_size
 
@@ -275,5 +280,11 @@ def gradient_descent_update(conf, params, grad_params):
         params: Updated parameter dictionary.
     """
     # TODO: Task 1.5
-    updated_params = None
+    eta = conf["learning_rate"]
+    updated_params = {}
+
+    for key, value in params.items():
+        dEdValue = grad_params["grad_"+key]
+        updated_params[key] = value - eta*dEdValue
+
     return updated_params

mandatory/m01/dnn/run.py
@@ -18,6 +18,7 @@ import numpy as np
 
 import model
 
+
 def get_batch_indices(indices, start_index, end_index):
     """Return the indices of the examples that are to form a batch.
 
@@ -33,6 +34,7 @@ def get_batch_indices(indices, start_index, end_index):
     """
     n = len(indices)
     return np.hstack((indices[start_index:min(n, end_index)], indices[0:max(end_index-n, 0)]))
+
 
 def train(conf, X_train, Y_train, X_devel, Y_devel):
     """Run training
@@ -75,13 +77,17 @@ def train(conf, X_train, Y_train, X_devel, Y_devel):
     print("Start training iteration")
     while True:
         start_time = time.time()
-        batch_indices = get_batch_indices(example_indices, batch_start_index, batch_end_index)
+        batch_indices = get_batch_indices(
+            example_indices, batch_start_index, batch_end_index)
         X_batch = X_train[:, batch_indices]
-        Y_batch = model.one_hot(Y_train[batch_indices], conf['output_dimension'])
+        Y_batch = model.one_hot(
+            Y_train[batch_indices], conf['output_dimension'])
 
-        Y_proposal, features = model.forward(conf, X_batch, params, is_training=True)
+        Y_proposal, features = model.forward(
+            conf, X_batch, params, is_training=True)
         cost_value, num_correct = model.cross_entropy_cost(Y_proposal, Y_batch)
-        grad_params = model.backward(conf, Y_proposal, Y_batch, params, features)
+        grad_params = model.backward(
+            conf, Y_proposal, Y_batch, params, features)
         params = model.gradient_descent_update(conf, params, grad_params)
 
         num_correct_since_last_check += num_correct
@@ -100,11 +106,11 @@ def train(conf, X_train, Y_train, X_devel, Y_devel):
             print("ERROR: nan encountered")
             break
 
-
         if step % conf['train_progress'] == 0:
             elapsed_time = time.time() - start_time
             sec_per_batch = elapsed_time / conf['train_progress']
-            examples_per_sec = conf['batch_size']*conf['train_progress'] / elapsed_time
+            examples_per_sec = conf['batch_size'] * \
+                conf['train_progress'] / elapsed_time
             ccr = num_correct / conf['batch_size']
             running_ccr = (num_correct_since_last_check /
                            conf['train_progress'] / conf['batch_size'])
@@ -120,7 +126,8 @@ def train(conf, X_train, Y_train, X_devel, Y_devel):
                                                                        sec_per_batch))
 
         if step % conf['devel_progress'] == 0:
-            num_correct, num_evaluated = evaluate(conf, params, X_devel, Y_devel)
+            num_correct, num_evaluated = evaluate(
+                conf, params, X_devel, Y_devel)
             devel_steps.append(step)
             devel_ccr.append(num_correct / num_evaluated)
             if conf['verbose']:
@@ -131,7 +138,8 @@ def train(conf, X_train, Y_train, X_devel, Y_devel):
             print("Terminating training after {} steps".format(step))
             break
 
-    train_progress = {'steps': train_steps, 'ccr': train_ccr, 'cost': train_cost}
+    train_progress = {'steps': train_steps,
+                      'ccr': train_ccr, 'cost': train_cost}
     devel_progress = {'steps': devel_steps, 'ccr': devel_ccr}
 
     return params, train_progress, devel_progress
@@ -157,7 +165,8 @@ def evaluate(conf, params, X_data, Y_data):
     end_ind = conf['batch_size']
     while True:
         X_batch = X_data[:, start_ind: end_ind]
-        Y_batch = model.one_hot(Y_data[start_ind: end_ind], conf['output_dimension'])
+        Y_batch = model.one_hot(
+            Y_data[start_ind: end_ind], conf['output_dimension'])
         Y_proposal, _ = model.forward(conf, X_batch, params, is_training=False)
         _, num_correct = model.cross_entropy_cost(Y_proposal, Y_batch)
         num_correct_total += num_correct